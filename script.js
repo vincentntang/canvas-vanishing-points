2ebaeac7f6bc2aced6907ee768704b281c68b95c
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-11-23 20:53:47 -0500

    Vertical Line straight down from center to bottom of canvas

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+var canvas = document.querySelector("canvas");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+canvas.width = 1000;
+canvas.height = 1000;
+
+var ct = canvas.getContext("2d");
+
+ct.beginPath();
+ct.moveTo(500, 500);
+ct.lineTo(500, 1000);
+ct.stroke();
