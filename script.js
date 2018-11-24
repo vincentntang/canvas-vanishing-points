d544f5ef876b1dc156cc0b4ff22019c8083f997e
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-11-23 21:05:11 -0500

    Initializing canvas to rationalize vanishing point

--- script.js
+++ script.js
@@ -8,7 +8,27 @@ canvas.height = 1000;
 
 var ct = canvas.getContext("2d");
 
-ct.beginPath();
-ct.moveTo(500, 500);
-ct.lineTo(500, 1000);
-ct.stroke();
+// TODO
+// 1. Make a center point
+// 2. Draw lines jutting from center
+// 3. Draw a line parallel to canvas bottom
+// 4. Draw an adjoining item upward
+
+
+function init() {
+  // First Line
+  ct.beginPath();
+  ct.moveTo(500, 0);
+  ct.lineTo(1000, 1000);
+  ct.stroke();
+
+  // Second Line
+  ct.beginPath();
+  ct.moveTo(500, 0);
+  ct.lineTo(0, 1000);
+  ct.stroke();
+
+}
+
+init();
+
