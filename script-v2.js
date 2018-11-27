1ac8db84edab876b58e870715e6be58abfeff491
Vincent <10290348+Kagerjay@users.noreply.github.com>
2018-11-26 19:11:07 -0500

    annotated -stackoverflow post code - doesn't create faces to color

--- script-v2.js
+++ script-v2.js
@@ -14,14 +14,15 @@ const scaleToFit = () => {
 
 var redraw = true; // When true, scene is redrawn ready for the next display refresh
 
-// Working with points
+// Constructor Function to initialize objects
 const point = (x = 0, y = 0) => ({ x, y });
-const pointCpy = (p, x = 0, y = 0) => ({ x: p.x + x, y: p.y + y });
-const scalePoint = (origin, point, scale) => {
+const pointCalc = (p, x = 0, y = 0) => ({ x: p.x + x, y: p.y + y });
+const scalePoint = (origin, point, scale) => { // y = mx + b slope formula for other points
   point.x = (point.x - origin.x) * scale + origin.x;
   point.y = (point.y - origin.y) * scale + origin.y;
 };
 
+// Objects Cartesion Points
 const p1 = point(400, 400);;
 const pA = point(p1.x, p1.y * 2);
 const pB = point(p1.x * 2, p1.y * 2);
@@ -38,14 +39,14 @@ function update() { // render loop redraws only if true
   if (redraw) { // monitor semaphore / state
     redraw = false; // clear semaphore / state
     ct.setTransform(1, 0, 0, 1, 0, 0); // resets transform
-    ct.clearRect(0, 0, ct.canvas.width, ct.canvas.height);
+    ct.clearRect(0, 0, ct.canvas.width, ct.canvas.height); // clear Canvas
     scaleToFit();
     draw();
   }
   requestAnimationFrame(update);
 }
 
-// was function init()
+// was function init(), specify start and endpoint
 function draw() {
   drawLine(p1, pA, "red");
   drawLine(p1, pB, "green");
@@ -55,25 +56,25 @@ function draw() {
 function drawVBox(p, size, vp, col, width) { // p is bottom left,  vp is vanish point
   ct.strokeStyle = col;
   ct.lineWidth = width;
-  const p0 = pointCpy(p); // get corners
-  const p1 = pointCpy(p, size);
-  const p2 = pointCpy(p, size, -size);
-  const p3 = pointCpy(p, 0, -size);
-  drawPoly(col, width, p0, p1, p2, p3);
+  const p0 = pointCalc(p); // get corners
+  const p1 = pointCalc(p, size, 0); // bottom right
+  const p2 = pointCalc(p, size, -size); // topright
+  const p3 = pointCalc(p, 0, -size); // topleft
+  drawPoly(col, width, p0, p1, p2, p3); // create frontface from values
 
   ct.beginPath(); // draw vanish lines
-  pathLine(p0, vp);
-  pathLine(p1, vp);
-  pathLine(p2, vp);
-  pathLine(p3, vp);
+  pathLine(p0, vp); // bottom left
+  pathLine(p1, vp); // bottom right
+  pathLine(p2, vp); // top right
+  pathLine(p3, vp); // top left
   ct.stroke();
 
   const scale = 1 - size / (800 * 2);
-  scalePoint(vp, p0, scale);
+  scalePoint(vp, p0, scale); // uses slope formula to calculate backface points
   scalePoint(vp, p1, scale);
   scalePoint(vp, p2, scale);
   scalePoint(vp, p3, scale);
-  drawPoly(col, width, p0, p1, p2, p3);
+  drawPoly(col, width, p0, p1, p2, p3); // create backface from values
 }
 
 // Use function to do common tasks and save yourself lots of typing
@@ -86,18 +87,20 @@ function drawLine(p1, p2, col, width = 1) {
   ct.stroke();
 }
 
+// For vanishing lines only
+function pathLine(p1, p2) {
+  ct.moveTo(p1.x, p1.y);
+  ct.lineTo(p2.x, p2.y);
+}
+
+// Draw the square
 function drawPoly(col, width, ...points) {
   ct.strokeStyle = col;
   ct.lineWidth = width;
   ct.beginPath();
   for (const p of points) {
-    ct.lineTo(p.x, p.y); // First point
+    ct.lineTo(p.x, p.y); // lineTo every point
   }
   ct.closePath(); // draw closing line
   ct.stroke();
-}
-
-function pathLine(p1, p2) {
-  ct.moveTo(p1.x, p1.y);
-  ct.lineTo(p2.x, p2.y);
 }
