3f227d32737cd43fa04419c3e4816e77a1ecd275
ACpython <10290348+Kagerjay@users.noreply.github.com>
2018-12-10 15:01:25 -0500

    Refactor - clean up github repo

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
-// var sliders = document.querySelectorAll("input");
-// sliders.forEach(item => {
-//   item.addEventListener('input', (e) => {
-//     item.nextElementSibling.textContent = e.target.value;
-//     console.log(item.getAttribute("data-tag"));
-//     window[item.getAttribute("data-tag")] = Number(e.target.value); // target variable by data-tag of input var name
-//     redraw = true; // uses semaphore (a state variable) to indicate when to redraw
-//   });
-// })
-
-// Init
 requestAnimationFrame(update);
 
 // Sliders
-var canvas: any = document.getElementById("canvas");
+var canvas = document.getElementById("canvas");
 var sliderSize = document.getElementById("sliderSize");
 var sliderOriginY = document.getElementById("sliderOriginY");
 var sliderOriginX = document.getElementById("sliderOriginX");
 var sliderToggleFill = document.getElementById("sliderToggleFill");
-sliderSize.addEventListener("input", (e) => {
-  delta = Number((e.target.value);
+sliderSize.addEventListener("input", e => {
+  delta = Number(e.target.value);
   sliderSize.nextElementSibling.textContent = e.target.value;
   redraw = true; // uses semaphore (a state variable) to indicate when to redraw
-})
-sliderOriginY.addEventListener("input", (e) => {
-  p1.y = Number((e.target.value);
+});
+sliderOriginY.addEventListener("input", e => {
+  p1.y = Number(e.target.value);
   sliderOriginY.nextElementSibling.textContent = e.target.value;
   redraw = true; // uses semaphore (a state variable) to indicate when to redraw
-})
-sliderOriginX.addEventListener("input", (e) => {
-  p1.x = Number((e.target.value);
+});
+sliderOriginX.addEventListener("input", e => {
+  p1.x = Number(e.target.value);
   sliderOriginX.nextElementSibling.textContent = e.target.value;
   redraw = true; // uses semaphore (a state variable) to indicate when to redraw
-})
-sliderToggleFill.addEventListener("input", (e) => {
-  toggleFill = Number((e.target.value);
+});
+sliderToggleFill.addEventListener("input", e => {
+  toggleFill = Number(e.target.value);
   sliderToggleFill.nextElementSibling.textContent = e.target.value;
   redraw = true; // uses semaphore (a state variable) to indicate when to redraw
-})
-
-
+});
 
 const ct = canvas.getContext("2d");
 const width = 1600;
@@ -50,7 +37,7 @@ canvas.height = window.innerHeight;
 const scaleToFit = () => {
   var scale = Math.min(canvas.width / width, canvas.height / height);
   ct.setTransform(scale, 0, 0, scale, 0, 0); // makes canvas fit to screen
-}
+};
 
 var redraw = true; // When true, scene is redrawn ready for the next display refresh
 
@@ -64,15 +51,17 @@ const scalePoint = (origin, point, scale) => {
 };
 
 // Objects Cartesion Points
-var p1 = point(400, 400);;
+var p1 = point(400, 400);
 var pA = point(p1.x, p1.y * 2);
 var pB = point(p1.x * 2, p1.y * 2);
 
 var delta = 50;
 var toggleFill = 0;
 
-function update() { // render loop redraws only if true
-  if (redraw) { // monitor semaphore / state
+function update() {
+  // render loop redraws only if true
+  if (redraw) {
+    // monitor semaphore / state
     redraw = false; // clear semaphore / state
     ct.setTransform(1, 0, 0, 1, 0, 0); // resets transform
     ct.clearRect(0, 0, ct.canvas.width, ct.canvas.height); // clear Canvas
@@ -89,16 +78,18 @@ function draw() {
   drawVBox(pB, delta, p1, "blue", 1);
 }
 
-function drawVBox(p, size, vp, col, width) { // p is bottom left,  vp is vanish point
+function drawVBox(p, size, vp, col, width) {
+  // p is bottom left,  vp is vanish point
   ct.strokeStyle = col;
-  ct.lineWidth = width; 0
+  ct.lineWidth = width;
+  0;
 
   const frontFace = [
     pointCalc(p),
     pointCalc(p, size, 0),
     pointCalc(p, size, -size),
     pointCalc(p, 0, -size)
-  ]
+  ];
   console.log(frontFace, "frontFace");
   const scale = 1 - size / (800 * 2); // Inverse scalar
   console.log(scale);
@@ -116,16 +107,11 @@ function drawVBox(p, size, vp, col, width) { // p is bottom left,  vp is vanish 
 }
 
 // sideface
-function makeFaceYZ(p, vp) {
-}
+function makeFaceYZ(p, vp) {}
 // front / backface
-function makeFaceXZ(p, vp) {
-
-}
+function makeFaceXZ(p, vp) {}
 // topface / bottomface
-function makeFaceXY(p, vp) {
-
-}
+function makeFaceXY(p, vp) {}
 
 function calculateSlope(p, vp) {
   return (p.y - vp.y) / (p.x - vp.x);
@@ -153,7 +139,7 @@ function drawPoly(col, width, ...points) {
   ct.strokeStyle = col;
   ct.lineWidth = width;
   if (toggleFill == 1) {
-    ct.fillStyle = 'lightgreen';
+    ct.fillStyle = "lightgreen";
   } else {
     ct.fillStyle = "rgba(0, 0, 200, 0)";
   }
